a391f068f0058ccf54f06fd3ad1f5509bdd13151
Milka Naliaka <milkawafula@gmail.com>
2020-11-30 12:45:52 +0300

    files update

---
+++ 3. Apache Spark and Spark Streaming/Lesson4/example_code_action.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf, SparkContext
+
+
+def action_exercise():
+    """
+    simple action exercise
+    :return:
+    """
+    # TODO create spark conf
+    conf =
+
+    # TODO create a spark context
+    sc =
+
+    # TODO create a simple rdd, parallelize a list of numbers
+    df = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    # TODO create a lambda function to get sum of all numbers in the list
+    df.reduce(
+
+    )
+
+
+if __name__ == "__main__":
+    action_exercise()
